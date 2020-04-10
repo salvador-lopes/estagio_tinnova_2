30d3dd670cb5b0280ffd51351d02590c63aff37d
Salvador <junior.salvador@tinnova.com.br>
2020-04-10 16:53:38 -0300

    removendo os primeiros logs

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ const session = require('express-session');
 const app = express();
 const bodyParser = require('body-parser');
 
-
-console.log("==> Iniciando aplicacao...");
-
-app.use((req, res, next) => {
-  console.log(">> Requisicao (" + req.method + "): " + req.path);
-  next();
-})
 app.use(session({ secret: 'estagio', saveUninitialized: true }));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}))
